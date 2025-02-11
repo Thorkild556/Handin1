7f42caa091796c259ecc42c9faa69d95b40ea21c
Thorkild Kappel <au716040@cas.au.dk>
2025-02-11 17:23:44 +0100

    more stack

--- handin1.c
+++ handin1.c
@@ -1,51 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+struct stack{
+    int capacity;
+    int *arr;
+    int height;
+};
+
+
+typedef struct stack stack;
+
+stack *stack_alloc(){
+
+    stack *p = malloc(sizeof(stack));
+    
+
+    *p = stack stack1{
+        .capacity = 2;
+        .arr = malloc(sizeof(int)*2);
+        .height = 0;
+    };
+
+    return p;
+}
+
+
+int stack_height(stack *s){
+    int height = (*s).height;
+    return height;
+}
+
+int stack_capacity(stack *s){
+    int capacity = (*s).capacity;
+    return capacity;
+}
+
+void stack_print(stack *s){
+    
+    stack st = *s;
+
+    for (int i = 0;  i < st.capacity, i++){
+        if (i < st.height)
+        {
+            printf("array = %d ", st.arr[i]);
+        }
+        else
+        {
+            printf(" . ");
+        }
+        
+        
+    }
+}
 
 int main(void){
 
-    struct stack{
-        int capacity;
-        int *arr;
-        int height;
-    };
-    
-    //struct stack stack1 = {.arr = {1,2}};
+    stack *stack1 = stack_alloc();
 
-    typedef struct stack stack;
-    
-    stack *stack_alloc(void){
-
-        stack *p = malloc(sizeof(stack));
-        
-
-        *p = stack stack2 = {
-            .capacity = 2;
-            .arr = malloc(sizeof(int)*2);
-            .height = 0;
-        };
-
-        return p;
-    }
-    
-
-    int stack_height(stack *s){
-        int height = (*s).height;
-        return height;
-    }
-
-    int stack_capacity(stack *s){
-        int capacity = (*s).capacity;
-        return capacity;
-    }
-
-    void stack_print(stack *s){
-        
-        stack st = *s;
-
-        for (int i = 0;  i < st.capacity, i++){
-            st.arr[i]
-            
-        }
-    }
+    stack_print(stack1);
 
 
 
