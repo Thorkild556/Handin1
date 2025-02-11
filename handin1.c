42a1b9b8c82e710c1b4a5121bd367c9f05d03d9c
Thorkild Kappel <au716040@cas.au.dk>
2025-02-11 17:07:15 +0100

    committed

--- handin1.c
+++ handin1.c
@@ -1,9 +1,51 @@
 #include <stdio.h>
 
 int main(void){
-    
-    struct stack;
 
+    struct stack{
+        int capacity;
+        int *arr;
+        int height;
+    };
+    
+    //struct stack stack1 = {.arr = {1,2}};
+
+    typedef struct stack stack;
+    
+    stack *stack_alloc(void){
+
+        stack *p = malloc(sizeof(stack));
+        
+
+        *p = stack stack2 = {
+            .capacity = 2;
+            .arr = malloc(sizeof(int)*2);
+            .height = 0;
+        };
+
+        return p;
+    }
+    
+
+    int stack_height(stack *s){
+        int height = (*s).height;
+        return height;
+    }
+
+    int stack_capacity(stack *s){
+        int capacity = (*s).capacity;
+        return capacity;
+    }
+
+    void stack_print(stack *s){
+        
+        stack st = *s;
+
+        for (int i = 0;  i < st.capacity, i++){
+            st.arr[i]
+            
+        }
+    }
 
 
 
