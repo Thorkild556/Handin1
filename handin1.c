aafca5465426496534c74fea0a9b6b83fe425d4c
Thorkild Kappel <au716040@cas.au.dk>
2025-02-15 17:33:25 +0100

    fixed the code...

--- handin1.c
+++ handin1.c
@@ -14,12 +14,11 @@ stack *stack_alloc(){
 
     stack *p = malloc(sizeof(stack));
     
+    // hej Benjamin, p->member er det samme som (*p).member når p er en pointer til en struct.
 
-    *p = stack stack1{
-        .capacity = 2;
-        .arr = malloc(sizeof(int)*2);
-        .height = 0;
-    };
+    p->capacity = 2;
+    p->arr = malloc(sizeof(int)*2);
+    p->height = 0;
 
     return p;
 }
@@ -37,12 +36,11 @@ int stack_capacity(stack *s){
 
 void stack_print(stack *s){
     
-    stack st = *s;
-
-    for (int i = 0;  i < st.capacity, i++){
-        if (i < st.height)
+    //har også ændret til -> her
+    for (int i = 0;  i < s->capacity; i++){
+        if (i < s->height)
         {
-            printf("array = %d ", st.arr[i]);
+            printf("array = %d ", s->arr[i]);
         }
         else
         {
